a7660c54b0f787f4edea58f00a380de10e48bd3f
luisarcher <luismcjordao@hotmail.com>
2016-05-16 22:34:25 +0100

    Item pick-up and collision OK

--- DungeonRPG/DRPG-Server/Jogador.c
+++ DungeonRPG/DRPG-Server/Jogador.c
@@ -3,7 +3,6 @@
 #include "Common.h"
 #include "Server.h"
 
-
 void NovoJogador(Jogador *j, int id) {
 	_tcscpy(j->nome, "guest");
 	j->hp = HP_BASE;
@@ -27,8 +26,7 @@ int MoverJogador(int playerId, int keystroke) {
 			if (j->y > 1)
 			{
 				gLabirinto.labirinto[j->y][j->x] = (hasMonsterAndPlayerIn(j->x, j->y) ? (gLabirinto.labirinto[j->y][j->x] / 100) : EMPTY);
-				j->y--;
-				gLabirinto.labirinto[j->y][j->x] = playerId;
+				if (!hasWallIn(j->x, j->y - 1) && !hasPlayerIn(j->x, j->y - 1)) j->y--;
 			}
 			break;
 		}
@@ -37,8 +35,7 @@ int MoverJogador(int playerId, int keystroke) {
 			if (j->y < LABIRINTOSIZE-2)
 			{
 				gLabirinto.labirinto[j->y][j->x] = (hasMonsterAndPlayerIn(j->x, j->y) ? (gLabirinto.labirinto[j->y][j->x] / 100) : EMPTY);
-				j->y++;
-				gLabirinto.labirinto[j->y][j->x] = playerId;
+				if (!hasWallIn(j->x, j->y + 1) && !hasPlayerIn(j->x, j->y + 1))j->y++;
 			}
 			break;
 		}
@@ -47,8 +44,7 @@ int MoverJogador(int playerId, int keystroke) {
 			if (j->x > 1)
 			{
 				gLabirinto.labirinto[j->y][j->x] = (hasMonsterAndPlayerIn(j->x, j->y) ? (gLabirinto.labirinto[j->y][j->x] / 100) : EMPTY);
-				j->x--;
-				gLabirinto.labirinto[j->y][j->x] = playerId;
+				if (!hasWallIn(j->x - 1, j->y) && !hasPlayerIn(j->x - 1, j->y)) j->y--; j->x--;
 			}
 			break;
 		}
@@ -57,16 +53,18 @@ int MoverJogador(int playerId, int keystroke) {
 			if (j->x < LABIRINTOSIZE - 2)
 			{
 				gLabirinto.labirinto[j->y][j->x] = (hasMonsterAndPlayerIn(j->x, j->y) ? (gLabirinto.labirinto[j->y][j->x] / 100) : EMPTY);
-				j->x++;
-				gLabirinto.labirinto[j->y][j->x] = playerId;
+				if (!hasWallIn(j->x + 1, j->y) && !hasPlayerIn(j->x + 1, j->y)) j->x++;
 			}
 			break;
 		}
 		default:
 		{
-			//Nothing to do here
+			_tprintf(TEXT("[SERVER] Não foi possível movimentar o jogador!"));
 			break;
 		}
+		if (hasObjectIn(j->x, j->y)) AskPlayerToCollectItems(j);
+		//Update matrix after collected items
+		gLabirinto.labirinto[j->y][j->x] = playerId;
 	}
 	return 0;
 }
@@ -78,30 +76,17 @@ void UpdatePlayerLOS(int x, int y, int(*matriz)[PLAYER_LOS]) {
 	//_tprintf(TEXT("POSX: %d POSY: %d\n\n"), x, y);
 	
 	int iniX, iniY, maxX,maxY;
-	if (x - 5 < 0)
-	{
-		iniX = 0;
-	}
-	else
-		iniX = x - 5;
-	if (x + 5 > LABIRINTOSIZE)
-	{
-		maxX = LABIRINTOSIZE;
-	}
-	else
-		maxX = x + 5;
-	if (y - 5 < 0)
-	{
-		iniY = 0;
-	}
-	else
-		iniY = y - 5;
-	if (y + 5 > LABIRINTOSIZE)
-	{
-		maxY = LABIRINTOSIZE;
-	}
-	else
-		maxY = y + 5;
+
+	if (x - 5 < 0) iniX = 0;
+	else iniX = x - 5;
+	if (x + 5 > LABIRINTOSIZE) maxX = LABIRINTOSIZE;
+	else maxX = x + 5;
+
+	if (y - 5 < 0) iniY = 0;
+	else iniY = y - 5;
+	if (y + 5 > LABIRINTOSIZE) maxY = LABIRINTOSIZE;
+	else maxY = y + 5;
+
 	int m=0, n= 0;
 	for (int i = iniY; i < maxY; i++)
 	{
@@ -113,24 +98,12 @@ void UpdatePlayerLOS(int x, int y, int(*matriz)[PLAYER_LOS]) {
 		n++;
 		m = 0;
 	}
-	/*for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			_tprintf(TEXT("%d"), matriz[j][i]);
-		}
-		_tprintf(TEXT("\n"));
-	}*/
 }
 
 void SetEmptyMatrix(int(*matriz)[PLAYER_LOS]) {
 	for (int i = 0; i < PLAYER_LOS; i++)
-	{
 		for (int j = 0; j < PLAYER_LOS; j++)
-		{
 			matriz[i][j] = FOG_OF_WAR;
-		}
-	}
 }
 
 void SetPlayerInRandomPosition(Jogador * p) {
@@ -156,6 +129,46 @@ BOOL hasMonsterAndPlayerIn(int x, int y) {
 	return gLabirinto.labirinto[y][x] > 1000;
 }
 
-//Collect items em mover
-//verificar se >1000 (monstros)
-//ler git
+BOOL hasObjectIn(int x, int y) {
+	return (gLabirinto.labirinto[y][x] >= ITEM_START_INDEX
+		&& gLabirinto.labirinto[y][x] <= ITEM_END_INDEX)
+		|| (gLabirinto.labirinto[y][x] > PEDRAS //Ver common.h
+		&& gLabirinto.labirinto[y][x] < 600);
+}
+
+BOOL hasWallIn(int x, int y) {
+	return gLabirinto.labirinto[y][x] >= WALL_START_INDEX
+		&& gLabirinto.labirinto[y][x] <= WALL_END_INDEX;
+}
+
+void AskPlayerToCollectItems(Jogador * p) {
+	int nPedras = 0;
+	/*
+	* Item que está nas cordenadas actuais do jogador,
+	* mas que o movimento ainda não tenha sido registado na matriz global
+	* para ele poder obter o objecto.
+	* Depois de apanhado, a posição do objecto na matriz,
+	* é sobreposta com o id do utilizador.
+	*/
+	switch (gLabirinto.labirinto[p->x][p->y])
+	{
+	case VITAMINA:
+		if ((p->hp + 1) <= (int)HP_BASE * 2) p->hp++;
+		break;
+	case ORANGE_BULL:
+		if ((p->hp + 1) <= (int)HP_BASE * 2) p->hp += 3;
+		break;
+	case REB_CAFEINA:
+		p->lentidao -= 2; //Efeito só deve de durar por 1 min (does not stack)
+		break;
+	default:
+		if ((nPedras = gLabirinto.labirinto[p->x][p->y]) > PEDRAS){
+			nPedras -= (int)PEDRAS;
+			if (p->nStones + nPedras <= (int)PLAYER_STONE_CAP) p->nStones += nPedras;
+		}
+		else {
+			_tprintf(TEXT("[SERVER] Falha ao apanhar objecto!"));
+		}
+		break;
+	}
+}
